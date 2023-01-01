e7b88948948db8aee3d2b240ddf7b9bac401bc0f
François Rullière <kapochamo@gmail.com>
2023-01-01 14:42:48 +0800

    shadow

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -4,13 +4,17 @@
 	// import github from "$lib/images/github.svg";
 	import cn from "classnames";
 	import Globe from "$lib/icons/globe.svelte";
+	let scrollY = 0;
 	const underlineAfter =
 		"after:origin-center relative  after:absolute after:h-[2px] after:inset-0 after:top-auto after:-bottom-1 after:transition-all after:scale-0 after:hover:scale-100";
 </script>
 
-<header class="top-0 left-0 right-0 fixed max-w-7xl mx-auto z-50">
+<svelte:window bind:scrollY />
+<header class="top-0 left-0 right-0 fixed max-w-6xl mx-auto z-50">
 	<nav
-		class="border-l border-t items-center border-gray-500  shadow-black shadow-md  bg-dark rounded-lg text-white mt-4 py-4 px-4 flex justify-between"
+		class="border-[0.5px] shadow-lg transition-all duration-700 items-center border-gray-500 shadow-black/40   bg-dark rounded-lg text-white mt-4 py-4 px-4 flex justify-between"
+		class:border-dark={scrollY === 0}
+		class:shadow-none={scrollY === 0}
 	>
 		<a href="/" class="text-2xl">Pastel</a>
 		<div class="group grid grid-flow-col gap-4 text-xl font-light">
